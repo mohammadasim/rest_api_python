dfe717bbb81f2c3357a7e77b3108ffe89ca30d32
Mohammad Ayub <mohammad.ayub@harpercollins.co.uk>
2018-08-09 09:13:45 +0100

    added extra code

--- flask_restful_section4/code/app.py
+++ flask_restful_section4/code/app.py
@@ -1,18 +1,32 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 '''
 The Api works with a resource and every resource needs to be a class
 The class needs to inherit from Resource class. Once the class is defined 
 it is added to the api as a resource
+Important point is that in Flask Restful we don't have to use jsonify as that is done for us.
 '''
 
 app = Flask(__name__)
 api = Api(app)
+items = []
 
-class Student(Resource):
+class Item(Resource):
     def get(self, name):
-        return {'student': name}
+        for item in items:
+            if item['name'] == name:
+                return item
+        return {'item': None}, 404
 
-api.add_resource(Student, '/student/<string:name>')
+    def post(self, name):
+        data = request.get_json()
+        item = {'name': name, 'price': data['price']}
+        items.append(item)
+        return item, 201
+class ItemList(Resource):
+    def get(self):
+        return{'items': items}
 
-app.run(port=5000)
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+app.run(port=5000, debug=True)
