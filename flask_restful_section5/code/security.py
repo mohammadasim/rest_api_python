8331a0a5ba8177e4ed879827928ba511e9df64ee
Mohammad Ayub <mohammad.ayub@harpercollins.co.uk>
2018-08-20 13:37:51 +0100

    modified code to use sqlite db

--- flask_restful_section5/code/security.py
+++ flask_restful_section5/code/security.py
@@ -9,16 +9,13 @@ In the username_mapping we first select the username and then use list comprehen
 the variable and values or attributes of the user object. so the result is bob: {id:1, username:'bob',password:'asdf'} we 
 have to remeber that when we create an object we assign to each attribute the value passed to it.
 '''
-username_mapping = { u.username: u for u in users}
-
-userid_mapping = {u.id: u for u in users}
 
 def authenticate(username, password):
     '''
     we have used the get() method to get value of a key from a dictionary.
     The benefit is using this is that we can return None if there isn't any match for the key
     '''
-    user = username_mapping.get(username, None)
+    user = User.find_by_username(username)
     #When comparing strings there can be issues with encodring etc. hence we user safe_str_cmp (safe string compare)
     if user and safe_str_cmp(user.password, password):
         return user
@@ -29,5 +26,5 @@ def identity(payload):
     library as a parameter and we have to extract the userid from it.
     '''
     user_id = payload['identity']
-    return userid_mapping.get(user_id, None)
+    return User.find_by_id(user_id)
 
--- flask_restful_section5/code/user.py
+++ flask_restful_section5/code/user.py
@@ -1,3 +1,4 @@
+import sqlite3
 class User:
     def __init__(self, _id, username, password):
         '''
@@ -6,4 +7,40 @@ class User:
         self.id = _id
         self.username = username
         self.password = password
+    '''    
+    @classmethod
+    def db_connection():
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+        return cursor
+    '''
+    @classmethod
+    def find_by_username(cls, username ):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+        select_query = "SELECT * FROM users WHERE username=?"
+        result = cursor.execute(select_query, (username,)) # The only way to pass a parameter, it has to be in the form of a tuple.
+        # the username is the form of a single tuple
+        row = result.fetchone() # fetchone() is a method of cursor object
+        if row:
+            user = cls(*row)
+        else:
+            user = None
+        connection.close()
+        return user
+
+    @classmethod
+    def find_by_id(cls, _id ):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+        select_query = "SELECT * FROM users WHERE id=?"
+        result = cursor.execute(select_query, (_id,)) # The only way to pass a parameter, it has to be in the form of a tuple.
+        # the username is the form of a single tuple
+        row = result.fetchone() # fetchone() is a method of cursor object
+        if row:
+            user = cls(*row)
+        else:
+            user = None
+        connection.close()
+        return user
     
